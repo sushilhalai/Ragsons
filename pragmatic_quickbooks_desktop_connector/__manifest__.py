e14d1c6543d2fe496f28315df0c70aa48c6dc2a0
Pragmatic <info@pragtech.co.in>
2021-04-21 11:07:27 +0530

    update invoice

--- pragmatic_quickbooks_desktop_connector/__manifest__.py
+++ pragmatic_quickbooks_desktop_connector/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo QuickBooks Desktop (QBD) Connector',
-    'version': '14.0.0.25',
+    'version': '14.0.0.26',
     'author': 'Pragmatic TechSoft Pvt Ltd.',
     'website': 'www.pragtech.co.in',
     'category': 'Sales, Invoice',
